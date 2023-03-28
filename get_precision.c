b6b13012b3a6e54c35e12e8c33422ee5bfa708c5
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-28 12:30:14 -0700

    Added get_precision.c File

--- get_precision.c
+++ get_precision.c
@@ -11,10 +11,9 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+		return (-1);
 
 	precision = 0;
 
